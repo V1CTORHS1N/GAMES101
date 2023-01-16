80370934bf75a0a356d0873f76c4aae607b030b6
Weitao Xin <xinweitao@live.cn>
2023-01-16 23:26:51 +1100

    commit homework 1

--- Homework1/Assignment1/main.cpp
+++ Homework1/Assignment1/main.cpp
@@ -6,6 +6,11 @@
 
 constexpr double MY_PI = 3.1415926;
 
+float DEG2RAD(float deg)
+{
+    return deg * MY_PI/180;
+}
+
 Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
 {
     Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
@@ -19,13 +24,73 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
     return view;
 }
 
-Eigen::Matrix4f get_model_matrix(float rotation_angle)
+Eigen::Matrix4f get_model_matrix(float rotation_angle, bool axis, const char **argv)
 {
     Eigen::Matrix4f model = Eigen::Matrix4f::Identity();
 
     // TODO: Implement this function
     // Create the model matrix for rotating the triangle around the Z axis.
     // Then return it.
+
+    /*
+    @author: Weitao Xin
+    @date: 2023-01-16
+    @brief: Affine Transformation Matrix
+    @details:
+    
+    Rotation around x-axis
+    [
+        [  1,           0,            0,            0 ],
+        [  0,          cos,         -sin,           0 ],
+        [  0,          sin,          cos,           0 ],
+        [  0,           0,            0,            1 ]
+    ]
+
+    Rotation around y-axis
+    [
+        [ cos,          0,           sin,           0 ],
+        [  0,           1,            0,            0 ],
+        [-sin,          0,           cos,           0 ],
+        [  0,           0,            0,            1 ]
+    ]
+
+    Rotation around z-axis
+    [
+        [ cos,        -sin,           0,            0 ],
+        [ sin,         cos,           0,            0 ],
+        [  0,           0,            1,            0 ],
+        [  0,           0,            0,            1 ]
+    ]
+    
+    */
+
+    rotation_angle = DEG2RAD(rotation_angle);
+    if (axis) {
+        if (!strcmp(argv[1], "-x") || !strcmp(argv[3], "-x"))
+        {
+            model(0, 0) = 1;
+            model(1, 1) = cos(rotation_angle);
+            model(1, 2) = -sin(rotation_angle);
+            model(2, 1) = sin(rotation_angle);
+            model(2, 2) = cos(rotation_angle);
+        }
+        else if (!strcmp(argv[1], "-y") || !strcmp(argv[3], "-y"))
+        {
+            model(0, 0) = cos(rotation_angle);
+            model(0, 2) = sin(rotation_angle);
+            model(1, 1) = 1;
+            model(2, 0) = -sin(rotation_angle);
+            model(2, 2) = cos(rotation_angle);
+        }
+    }
+    else
+    {
+        model(0, 0) = cos(rotation_angle);
+        model(0, 1) = -sin(rotation_angle);
+        model(1, 0) = sin(rotation_angle);
+        model(1, 1) = cos(rotation_angle);
+        model(2, 2) = 1;
+    }
 
     return model;
 }
@@ -41,20 +106,63 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
     // Create the projection matrix for the given parameters.
     // Then return it.
 
+    /*
+    @author: Weitao Xin
+    @date: 2023-01-16
+    @brief: Perspective Projection Matrix
+    @details:
+    [
+        [ n/r,          0,            0,            0      ],
+        [  0,          n/t,           0,            0      ],
+        [  0,           0,       (n+f)/(n-f),   -2nf/(n-f) ],
+        [  0,           0,            1,            0      ]
+    ]
+    */
+
+    float t = -tanh(DEG2RAD(eye_fov) / 2) * abs(zNear);
+    float r = t / aspect_ratio;
+
+    projection(0, 0) = zNear / r;
+    projection(1, 1) = zNear / t;
+    projection(2, 2) = (zNear + zFar) / (zNear - zFar);
+    projection(2, 3) = -2 * zNear * zFar / (zNear - zFar);
+    projection(3, 2) = 1;
+
     return projection;
 }
 
-int main(int argc, const char** argv)
+int main(int argc, const char **argv)
 {
     float angle = 0;
     bool command_line = false;
+    bool axis = false;
     std::string filename = "output.png";
 
-    if (argc >= 3) {
-        command_line = true;
-        angle = std::stof(argv[2]); // -r by default
-        if (argc == 4) {
-            filename = std::string(argv[3]);
+    if (argc == 2)
+    {
+        if (!strcmp(argv[1], "-x") || !strcmp(argv[1], "-y"))
+        {
+            axis = true;
+        }
+    }
+    else if (argc >= 3)
+    {
+        if (!strcmp(argv[1], "-r"))
+        {
+            command_line = true;
+            angle = std::stof(argv[2]); // -r by default
+            if (!strcmp(argv[3], "-x") || !strcmp(argv[3], "-y"))
+            {
+                axis = true;
+            }
+            if (argc == 4)
+            {
+                filename = std::string(argv[3]);
+            } 
+            else if (argc == 5)
+            {
+                filename = std::string(argv[4]);
+            }
         }
     }
 
@@ -72,10 +180,11 @@ int main(int argc, const char** argv)
     int key = 0;
     int frame_count = 0;
 
-    if (command_line) {
+    if (command_line)
+    {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
-        r.set_model(get_model_matrix(angle));
+        r.set_model(get_model_matrix(angle, axis, argv));
         r.set_view(get_view_matrix(eye_pos));
         r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
 
@@ -88,10 +197,11 @@ int main(int argc, const char** argv)
         return 0;
     }
 
-    while (key != 27) {
+    while (key != 27)
+    {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
-        r.set_model(get_model_matrix(angle));
+        r.set_model(get_model_matrix(angle, axis, argv));
         r.set_view(get_view_matrix(eye_pos));
         r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
 
@@ -102,12 +212,15 @@ int main(int argc, const char** argv)
         cv::imshow("image", image);
         key = cv::waitKey(10);
 
-        std::cout << "frame count: " << frame_count++ << '\n';
+        std::cout << "frame count: " << frame_count++ << '\r';
+        fflush(stdout);
 
-        if (key == 'a') {
+        if (key == 'a')
+        {
             angle += 10;
         }
-        else if (key == 'd') {
+        else if (key == 'd')
+        {
             angle -= 10;
         }
     }
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # GAMES101
-This is an archive for homework in course GAMES101.
+> This is an archive for homework in course GAMES101.
+
+## Homework 1
+### Running command
+```bash
+./Rasterizer                    #Turning around z-axis (by default)
+
+./Rasterizer -x                 #Turning around x-axis
+
+./Rasterizer -y                 #Turning around y-axis
+
+./Rasterizer -r 20              #Turning the triangle around z-axis by 20 degree
+                                #save it to output.png
+
+./Rasterizer -r 20 image.png    #Turning the triangle around z-axis by 20 degree
+                                #save it to image.png
+
+./Rasterizer -r 20 -x image.png #Turning the triangle around x-axis by 20 degree
+                                #save it to image.png
+```
+### Basic
+Turning around z-axis
+![basic](./images/homework1-basic.gif)
+
+### Advance
+Turning around x-axis
+![advance-x](images/homework1-advance-x.gif)
+
+Turning around y-axis
+![advance-y](./images/homework1-advance-y.gif)
